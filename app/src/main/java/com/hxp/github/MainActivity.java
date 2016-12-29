7f40aa6a6b965cf43a958059b389cd6fba2ed294
thinkpad <258205965@qq.com>
2016-12-29 14:53:40 +0800

    全部2

--- app/src/main/java/com/hxp/github/MainActivity.java
+++ app/src/main/java/com/hxp/github/MainActivity.java
@@ -3,11 +3,14 @@ package com.hxp.github;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.com.com.Com;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Com.com();
     }
 }
